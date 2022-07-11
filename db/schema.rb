6356904d988c696a382aa23cdfdd0578de11d367
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-11 15:05:13 -0400

    Adds first and last names to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_11_185126) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_11_190307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_11_185126) do
     t.string "handle"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
